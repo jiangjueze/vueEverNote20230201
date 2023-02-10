3bee3f15bf036d7766bb63ce4e94fd4c107a5458
jiangjueze <931914369@qq.com>
2023-02-10 15:49:38 +0800

    [feat]笔记本详情改进只剩1节user模块了

--- src/components/NoteSidebar.vue
+++ src/components/NoteSidebar.vue
@@ -1,13 +1,22 @@
 <template>
   <div class="note-sidebar">
-    <span class="btn add-note" @click="addNote" >添加笔记</span>
-    <el-dropdown class="notebook-title"  @command="handleCommand" placement="bottom">
+    <span class="btn add-note" @click="onAddNote">添加笔记</span>
+    <el-dropdown
+      class="notebook-title"
+      @command="handleCommand"
+      placement="bottom"
+    >
       <span class="el-dropdown-link">
-        {{curBook.title}} <i class="iconfont icon-down"></i>
+        {{ curBook.title }} <i class="iconfont icon-down"></i>
       </span>
       <el-dropdown-menu slot="dropdown">
-        <el-dropdown-item v-for="notebook in notebooks" :command="notebook.id" :key="notebook.id">{{notebook.title}}</el-dropdown-item>
-        <el-dropdown-item  command="trash">回收站</el-dropdown-item>
+        <el-dropdown-item
+          v-for="notebook in notebooks"
+          :command="notebook.id"
+          :key="notebook.id"
+          >{{ notebook.title }}</el-dropdown-item
+        >
+        <el-dropdown-item command="trash">回收站</el-dropdown-item>
       </el-dropdown-menu>
     </el-dropdown>
     <div class="menu">
@@ -17,8 +26,8 @@
     <ul class="notes">
       <li v-for="note in notes" :key="note.id">
         <router-link :to="`/note?noteId=${note.id}&notebookId=${curBook.id}`">
-          <span class="date">{{note.updatedAtFriendly}}</span>
-          <span class="title">{{note.title}}</span>          
+          <span class="date">{{ note.updatedAtFriendly }}</span>
+          <span class="title">{{ note.title }}</span>
         </router-link>
       </li>
     </ul>
@@ -26,67 +35,84 @@
 </template>
 
 <script>
-  import Notebooks from '@/apis/notebooks'
-  import Notes from '@/apis/notes'
-  import Bus from '@/helpers/bus'
+import Notebooks from "@/apis/notebooks";
+import Notes from "@/apis/notes";
+import Bus from "@/helpers/bus";
+import { mapState, mapGetters, mapMutations, mapActions } from "vuex";
 
-  export default {
-    created() {
-      Notebooks.getAll()
-        .then(res => {
-          this.notebooks = res.data
-          this.curBook = this.notebooks.find(notebook => notebook.id == this.$route.query.notebookId)
-            || this.notebooks[0] || {}
-          return Notes.getAll({ notebookId: this.curBook.id })
-        }).then(res => {
-          this.notes = res.data
-          this.$emit('update:notes', this.notes)
-          // this.$emit('test', this.notes)
-          Bus.$emit('update:note', this.notes)
-        })
+export default {
+  created() {
+    this.getNotebooks()
+      .then(() => {
+        // this.$store.commit("setCurBook", {
+        //   curBookId: this.$route.query.notebookId
+        // });
+        this.setCurBook({ curBookId: this.$route.query.notebookId });
+        // console.log(this.curBook, "111");
+        return this.getNotes({ notebookId: this.curBook.id });
+      })
+      .then(() => {
+        // this.$store.commit("setCurNote", {
+        //   curNoteId: this.$route.query.noteId
+        // });
+        this.setCurNote({ curNoteId: this.$route.query.noteId });
+      });
+
+    // Notebooks.getAll()
+    //   .then(res => {
+    //     this.notebooks = res.data;
+    //     this.curBook =
+    //       this.notebooks.find(
+    //         notebook => notebook.id == this.$route.query.notebookId
+    //       ) ||
+    //       this.notebooks[0] ||
+    //       {};
+    //     return Notes.getAll({ notebookId: this.curBook.id });
+    //   })
+    //   .then(res => {
+    //     this.notes = res.data;
+    //     this.$emit("update:notes", this.notes);
+    //     // this.$emit('test', this.notes)
+    //     Bus.$emit("update:note", this.notes);
+    //   });
+  },
+
+  data() {
+    return {};
+  },
+
+  computed: {
+    ...mapGetters(["notebooks", "notes", "curBook"])
+  },
+
+  methods: {
+    ...mapMutations(["setCurBook", "setCurNote"]),
+    ...mapActions(["getNotebooks", "getNotes", "addNote"]),
+
+    handleCommand(notebookId) {
+      if (notebookId == "trash") {
+        return this.$router.push({ path: "/trash" });
+      }
+      this.$store.commit("setCurBook", { curBookId: notebookId });
+      // this.curBook = this.notebooks.find(notebook => notebook.id == notebookId);
+      this.getNotes({ notebookId }).then(() => {
+        console.log(notebookId, this.notes);
+      });
+
+      // Notes.getAll({ notebookId }).then(res => {
+      //   this.notes = res.data;
+      //   this.$emit("update:notes", this.notes);
+      //   Bus.$emit("update:note", this.notes);
+      // });
     },
 
-    data() {
-      return {
-        notebooks: [],
-        notes:[],
-        curBook: {}
-      }
-    },
-
-    methods: {
-      handleCommand(notebookId) {
-        console.log(notebookId,this.notes)
-        if(notebookId == 'trash') {
-          return this.$router.push({ path: '/trash'})
-        }
-        this.curBook = this.notebooks.find(notebook => notebook.id == notebookId)
-        
-        Notes.getAll({ notebookId })
-          .then(res => {
-            this.notes = res.data
-            this.$emit('update:notes', this.notes)
-            Bus.$emit('update:note', this.notes)
-          })
-      },
-
-      addNote() {
-        Notes.addNote({ notebookId: this.curBook.id })
-          .then(res => {
-            console.log(res)
-            this.notes.unshift(res.data)
-          })
-      }
-
+    onAddNote() {
+      this.addNote({ notebookId: this.curBook.id });
     }
   }
-
+};
 </script>
 
-
-<style lang="less" >
+<style lang="less">
 @import url(../assets/css/note-sidebar.less);
-
 </style>
-
-
---
+++ src/store/modules/note.js
@@ -0,0 +1,84 @@
+import Note from "@/apis/notes";
+import { Message } from "element-ui";
+
+const state = {
+  notes: null,
+  curNoteId: null
+};
+
+const getters = {
+  notes: state => state.notes || [],
+  curNote: state => {
+    if (!Array.isArray(state.notes)) {
+      return {};
+    }
+    if (!state.curNoteId) {
+      return state.notes[0] || {}; // 不加这个容易报错，当notebook为[]的时候
+    }
+    return state.notes.find(note => note.id == state.curNoteId) || {};
+  }
+};
+
+const mutations = {
+  setNote(state, payload) {
+    state.notes = payload.notes;
+  },
+
+  addNote(state, payload) {
+    state.notes.unshift(payload.note);
+  },
+
+  updateNote(state, payload) {
+    let note = state.notes.find(note => note.id === payload.noteId) || {};
+    note.title = payload.title;
+    note.content = payload.content;
+  },
+
+  deleteNote(state, payload) {
+    state.notes = state.notes.filter(note => note.id !== payload.noteId);
+  },
+
+  setCurNote(state, payload) {
+    state.curNoteId = payload.curNoteId;
+  }
+};
+
+const actions = {
+  getNotes({ commit }, { notebookId }) {
+    return Note.getAll({ notebookId }).then(res => {
+      commit("setNote", { notes: res.data });
+    });
+  },
+
+  addNote({ commit }, { notebookId, title, content }) {
+    return Note.addNote({ notebookId }, { title, content }).then(res => {
+      commit("addNote", { note: res.data });
+      // Message.success(res.msg);
+    });
+  },
+
+  updateNote({ commit }, { noteId, title, content }) {
+    return Note.updateNote({ noteId }, { title, content }).then(res => {
+      commit("updateNote", {
+        noteId,
+        title,
+        content
+      });
+      // Message.success(res.msg);
+    });
+  },
+
+  deleteNote({ commit }, { noteId }) {
+    return Note.deleteNote({ noteId }).then(res => {
+      commit("deleteNotebook", { noteId });
+      Message.success(res.msg);
+    });
+  }
+};
+
+export default {
+  state,
+  getters,
+  mutations,
+  actions
+};
